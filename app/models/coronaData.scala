ea93341bb89668b263a6725829a8330e45a1bbc6
karamveer-scala <58160005+karamveer-scala@users.noreply.github.com>
2020-04-19 20:11:55 +0530

    Update coronaData.scala
    removed side header

--- app/models/coronaData.scala
+++ app/models/coronaData.scala
@@ -49,7 +49,26 @@ for(i <- x){
         
         
          val data="\n  "+i._1 +" : "+i._2._1+ ", "+i._2._2
-         val d=(i._1,i._2._1.toString(),i._2._2.toString())
+         
+         val country=i._1.replace("United_States_of_America", "USA")
+                         .replace("United_Kingdom", "UK")
+                         .replace("Bosnia_and_Herzegovina", "B&H")
+                         .replace("United_Arab_Emirates", "UAE")
+                         .replace("Democratic_Republic_of_the_Congo", "DROC")
+                         .replace("Cases_on_an_international_conveyance_Japan", "Int_conveyance_Japan")
+                         .replace("United_Republic_of_Tanzania", "Tanzanian")
+                         .replace("Saint_Vincent_and_the_Grenadines", "Saint Vincent")
+                          .replace("Falkland_Islands_(Malvinas)", "Falkland_Islands")
+                           .replace("Central_African_Republic", "Central_Africa")
+                            .replace("Sao_Tome_and_Principe", "Sao_Tome&Principe")
+                             .replace("Turks_and_Caicos_islands", "Turks&Caicos")
+                              .replace("United_States_Virgin_Islands", "U.S Virgin Islands")
+                               .replace("", "")
+                                .replace("", "")
+                                
+        // .map(_.replaceAll("United_States_of_America", "USA"))
+         
+         val d=(country,i._2._1.toString(),i._2._2.toString())
           listBuff += (d)
         }
           listper=listBuff.toList
@@ -70,10 +89,24 @@ for(i <- x){
        
        
         for(i <- sort){
-        
+        val country=i._1.replace("United_States_of_America", "USA")
+                         .replace("United_Kingdom", "UK")
+                         .replace("Bosnia_and_Herzegovina", "B&H")
+                         .replace("United_Arab_Emirates", "UAE")
+                         .replace("Democratic_Republic_of_the_Congo", "DROC")
+                         .replace("Cases_on_an_international_conveyance_Japan", "Int_conveyance_Japan")
+                         .replace("United_Republic_of_Tanzania", "Tanzanian")
+                         .replace("Saint_Vincent_and_the_Grenadines", "Saint Vincent")
+                          .replace("Falkland_Islands_(Malvinas)", "Falkland_Islands")
+                           .replace("Central_African_Republic", "Central_Africa")
+                            .replace("Sao_Tome_and_Principe", "Sao_Tome&Principe")
+                             .replace("Turks_and_Caicos_islands", "Turks&Caicos")
+                              .replace("United_States_Virgin_Islands", "U.S Virgin Islands")
+                               .replace("", "")
+                                .replace("", "")
         
          val data="\n  "+i._1 +" : "+i._2._1+ ", "+i._2._2
-         val d=(i._1,i._2._1.toString(),i._2._2.toString())
+         val d=(country,i._2._1.toString(),i._2._2.toString())
           listBuff += (d)
         }
           listper=listBuff.toList
