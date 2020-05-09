6cb012b69da6001d8acdce4a4455c375c0349c03
karamveer-scala <58160005+karamveer-scala@users.noreply.github.com>
2020-05-09 14:18:24 +0530

    Update chart.scala

--- app/com/spark/corona/chart.scala
+++ app/com/spark/corona/chart.scala
@@ -16,6 +16,10 @@ import play.api.Play
  import java.lang.ClassLoader
 import play.api.libs.json.JsObject
 import play.api.libs.json.Json
+import scala.collection.mutable.Buffer
+import org.omg.CORBA._PolicyStub
+import java.util.stream.Collectors
+import java.util.Arrays
 
 
 object chart {
@@ -23,11 +27,11 @@ object chart {
   
   
   
-  def trendCase(p_country: String) :(String,String,String)={
+  def trendCase(p_country: String) :(String,String,String,String)={
             val url="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
             var data=scala.io.Source.fromURL(url).getLines().toArray
            
-          
+         //  println(" \n first DATA :  \n\n"+data.mkString(",")) 
             
             val dateser=data.take(1).mkString(",").split(",")
             var ndateser=  dateser.toBuffer
@@ -49,33 +53,69 @@ object chart {
             }
             listdate = listBuffdate.toList
             
-          //  for(x <- listdate) println(x)
-            
         
-            var cntrydata=data.filter(_.contains(p_country)).toBuffer
-             
-          //  cntrydata -= ("Country/Region")//.drop(1)
+       
             
-            var length=cntrydata.mkString(",").split(",").toBuffer
-            
-            length -= (length(0),length(1),length(2),length(3))
-            
-            
+                    var karam=data.filter(_.contains(p_country))
+         
+      
+           var a :Buffer[String] =null
+            var listBuff = new ListBuffer[String] 
+                 for(i <-  karam)  { 
+                    a=i.split(",").toBuffer
+                   a -= (a(0),a(1),a(2),a(3))
+                  
+                   listBuff+=a.mkString(",")
+                   
+                 }
               
-               var contrydata=length.toList
+            var list=listBuff.toList
+      
+             
+     
+             
+            val donutsGroup = list
+     
+            val cnd=   donutsGroup.map(_.split(",").map(_.toInt).toList)
+      
+         
+            val sum=   cnd.transpose.map(_.sum) 
+       
+      
+           
+                 var contrydata= sum //length.toList
+              var incre="" 
+      
+               var list1 = contrydata.toBuffer
+               var list2 = contrydata.toBuffer 
+               var len=list2.length 
+               list2 -=(list2(len-1) ) 
+
+               list2= (0 +: list2)//.map(_.toString())
+               var sub=list1 zip list2 map(x=>x._1.toInt-x._2.toInt) 
                
+              incre=sub.mkString(",")
+  
+               
+               
+               
+               
+               
+
                var chartdata=contrydata.mkString(",")
+   
+               
                var strDate=listdate.head.split("-")
                var (mnt,day,yr)=(strDate(0),strDate(1),strDate(2))
                  
         var passdate=listdate.head
         var lastdate = listdate.last  
               
-      (passdate,lastdate,chartdata)
+      (passdate,lastdate,chartdata,incre)
      }
   
   // Death
-   def trendDeath(p_country: String) :(String,String,String)={
+   def trendDeath(p_country: String) :(String,String,String,String)={
             val url="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
             var data=scala.io.Source.fromURL(url).getLines().toArray
            
@@ -101,20 +141,51 @@ object chart {
             }
             listdate = listBuffdate.toList
             
-          //  for(x <- listdate) println(x)
-        
-            var cntrydata=data.filter(_.contains(p_country)).toBuffer
-             
-          //  cntrydata -= ("Country/Region")//.drop(1)
-            
-            var length=cntrydata.mkString(",").split(",").toBuffer
-            
-            length -= (length(0),length(1),length(2),length(3))
-            
-            
+                var karam=data.filter(_.contains(p_country))
+         
+      
+           var a :Buffer[String] =null
+            var listBuff = new ListBuffer[String] 
+                 for(i <-  karam)  { 
+                    a=i.split(",").toBuffer
+                   a -= (a(0),a(1),a(2),a(3))
+                  
+                   listBuff+=a.mkString(",")
+                   
+                 }
               
-               var contrydata=length.toList
+            var list=listBuff.toList
+      
+             
+     
+             
+            val donutsGroup = list
+     
+            val cnd=   donutsGroup.map(_.split(",").map(_.toInt).toList)
+      
+         
+            val sum=   cnd.transpose.map(_.sum) 
+      
+           
+                 var contrydata= sum //length.toList
+              var incre="" 
+          
+               var list1 = contrydata.toBuffer
+               var list2 = contrydata.toBuffer 
+               var len=list2.length 
+               list2 -=(list2(len-1) ) 
+
+               list2= (0 +: list2)//.map(_.toString())
+               var sub=list1 zip list2 map(x=>x._1.toInt-x._2.toInt) 
                
+              incre=sub.mkString(",")
+  
+               
+               
+               
+               
+               
+
                var chartdata=contrydata.mkString(",")
                var strDate=listdate.head.split("-")
                var (mnt,day,yr)=(strDate(0),strDate(1),strDate(2))
@@ -122,13 +193,13 @@ object chart {
         var passdate=listdate.head
         var lastdate = listdate.last  
               
-      (passdate,lastdate,chartdata)
+      (passdate,lastdate,chartdata,incre)
      }
   
   
   //REcover
    
-    def trendRecover(p_country: String) :(String,String,String)={
+    def trendRecover(p_country: String) :(String,String,String,String)={
             val url="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
             var data=scala.io.Source.fromURL(url).getLines().toArray
            
@@ -154,20 +225,53 @@ object chart {
             }
             listdate = listBuffdate.toList
             
-          //  for(x <- listdate) println(x)
-        
-            var cntrydata=data.filter(_.contains(p_country)).toBuffer
-             
-          //  cntrydata -= ("Country/Region")//.drop(1)
-            
-            var length=cntrydata.mkString(",").split(",").toBuffer
-            
-            length -= (length(0),length(1),length(2),length(3))
-            
-            
+                  var karam=data.filter(_.contains(p_country))
+         
+      
+           var a :Buffer[String] =null
+            var listBuff = new ListBuffer[String] 
+                 for(i <-  karam)  { 
+                    a=i.split(",").toBuffer
+                   a -= (a(0),a(1),a(2),a(3))
+                  
+                   listBuff+=a.mkString(",")
+                   
+                 }
               
-               var contrydata=length.toList
+            var list=listBuff.toList
+      
+             
+     
+             
+            val donutsGroup = list
+     
+            val cnd=   donutsGroup.map(_.split(",").map(_.toInt).toList)
+      
+         
+            val sum=   cnd.transpose.map(_.sum) 
+       
+       
+  
+           
+                 var contrydata= sum //length.toList
+              var incre="" 
+       
+               var list1 = contrydata.toBuffer
+               var list2 = contrydata.toBuffer 
+               var len=list2.length 
+               list2 -=(list2(len-1) ) 
+
+               list2= (0 +: list2)//.map(_.toString())
+               var sub=list1 zip list2 map(x=>x._1.toInt-x._2.toInt) 
                
+              incre=sub.mkString(",")
+  
+               
+               
+               
+               
+               
+
                var chartdata=contrydata.mkString(",")
                var strDate=listdate.head.split("-")
                var (mnt,day,yr)=(strDate(0),strDate(1),strDate(2))
@@ -175,7 +279,7 @@ object chart {
         var passdate=listdate.head
         var lastdate = listdate.last  
               
-      (passdate,lastdate,chartdata)
+      (passdate,lastdate,chartdata,incre)
      }
   
   
